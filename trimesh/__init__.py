ed9ee4a6a38a1c2990290824f5c169de084fab44
Michael Dawson-Haggerty <mik3dh@gmail.com>
2015-03-20 23:48:23 -0400

    renamed load

--- trimesh/__init__.py
+++ trimesh/__init__.py
@@ -18,6 +18,6 @@ if _version_info[:2] < (2, 7):
 
 from .base import Trimesh
 from .geometry import unitize, transform_points
-from .io.base import load_mesh, available_formats
+from .io.load import load_mesh, available_formats
 from . import transformations
 
