baad7b649d91fa8e92fc67a60b256f18be4a5f87
Michael Dawson-Haggerty <mik3dh@gmail.com>
2015-03-31 14:38:39 -0400

    modified setup to include io

--- setup.py
+++ setup.py
@@ -3,11 +3,13 @@
 from distutils.core import setup
 
 setup(name='trimesh',
-      version='1.0.3',
+      version='1.0.4',
       description='Load, process, and view triangular meshes.',
       author='Mike Dawson-Haggerty',
       author_email='mik3dh@gmail.com',
       url='github.com/mikedh/trimesh.py',
-      packages=['trimesh'],
+      packages=['trimesh',
+                'trimesh.io',
+                'trimesh.ray'],
       install_requires=['numpy', 'scipy', 'networkx', 'pyglet']
      )
