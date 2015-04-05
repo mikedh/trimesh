d9f3ff9c626722a442ef0ae114ecbaa0af8ee0b1
Michael Dawson-Haggerty <mik3dh@gmail.com>
2015-04-05 12:31:23 -0400

    ray cleanup

--- trimesh/ray/ray_triangle_cpu.py
+++ trimesh/ray/ray_triangle_cpu.py
@@ -4,11 +4,7 @@ Narrow phase ray- triangle intersection
 import numpy as np
 import time
 
-from ..constants import log, log_time
-
-from collections import deque
-
-TOL_ZERO    = 1e-12
+from ..constants import log, log_time, TOL_ZERO
 
 @log_time
 def rays_triangles_id(triangles,
@@ -16,17 +12,22 @@ def rays_triangles_id(triangles,
                       ray_candidates = None,
                       return_any     = False):
     '''
+    Intersect a set of rays and triangles. 
+
     Arguments
     ---------
     triangles:      (n, 3, 3) float array of triangle vertices
     rays:           (m, 2, 3) float array of ray start, ray directions
     ray_candidates: (m, *) int array of which triangles are candidates
                     for the ray. 
-    
+    return_any:     bool flag for output format
+
     Returns
     ---------
-    intersections:  (m) bool of whether the ray hit any triangles
+    intersections, return_any:      (m) bool of whether the ray hit any triangles
+    intersections, not return_any:  (m) sequence of triangle indexes hit by rays
     '''
+
     if return_any: hits = np.zeros(len(rays), dtype=np.bool)
     else:          hits = [None] * len(rays)
   
@@ -35,16 +36,17 @@ def rays_triangles_id(triangles,
             triangle_candidates = triangles
         else: 
             triangle_candidates = triangles[ray_candidates[ray_index]]
-        hit = ray_triangles_vec(triangle_candidates, *ray)
+        hit = ray_triangles_vectorized(triangle_candidates, *ray)
         if return_any: hits[ray_index] = hit.sum() > 0
         else:          hits[ray_index] = np.nonzero(hit)[0]
     return hits
 
-def ray_triangles(triangles, 
-                 ray_origin, 
-                 ray_direction):
+def ray_triangles_loop(triangles, 
+                       ray_origin, 
+                       ray_direction):
     '''
-    Stub for actually vectorizing ray_triangle properly
+    Intersection of multiple triangles and a single ray. 
+    Roughly 10x slower than ray_triangles_vectorized
     '''
     for triangle in triangles:
         if ray_triangle(triangle, ray_origin, ray_direction):
@@ -56,8 +58,10 @@ def ray_triangle(triangle,
                  ray_direction):
     '''
     Intersection test for a single ray and a single triangle
+
+    Uses Moller-Trumbore intersection algorithm
     '''
-    #http://en.wikipedia.org/wiki/M%C3%B6ller%E2%80%93Trumbore_intersection_algorithm
+    
 
     edges = [triangle[1] - triangle[0],
              triangle[2] - triangle[0]]
@@ -85,17 +89,14 @@ def ray_triangle(triangle,
         return True
     return False
 
-def _diag_dot(a, b):
-    result = np.array([np.dot(i,j) for i,j in zip(a,b)])
-    return result
-    #return np.diag(np.dot(a,b))
+def ray_triangles_vectorized(triangles, 
+                             ray_origin, 
+                             ray_direction):
+    '''
+    Intersection of multiple triangles and a single ray.
 
-def ray_triangles_vec(triangles, 
-                      ray_origin, 
-                      ray_direction):
-    
-    #http://en.wikipedia.org/wiki/M%C3%B6ller%E2%80%93Trumbore_intersection_algorithm
-
+    Uses Moller-Trumbore intersection algorithm
+    '''
     candidates = np.ones(len(triangles), dtype=np.bool)
 
     vert0 = triangles[:,0,:]
@@ -142,4 +143,13 @@ def ray_triangles_vec(triangles,
     candidates[candidates] = t > TOL_ZERO
 
     return candidates
-    
+
+def _diag_dot(a, b):
+    '''
+    Dot product by row of a and b.
+
+    Same as np.diag(np.dot(a, b.T)) but without the monstrous 
+    intermediate matrix (and is much faster). 
+    '''
+    result = np.array([np.dot(i,j) for i,j in zip(a,b)])
+    return result
