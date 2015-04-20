2780276a96523d64fe7af3152067b0234e7c5e6c
Michael Dawson-Haggerty <mik3dh@gmail.com>
2015-04-19 23:13:02 -0400

    added some examples

---
+++ examples/convexify.py
@@ -0,0 +1,33 @@
+'''
+examples/convexify.py
+
+Take a mesh with multiple bodies, take the convex hull
+of each body, then combine them back into one mesh. 
+Useful for generating collision models of an object. 
+'''
+
+
+import trimesh
+import numpy as np
+
+if __name__ == '__main__':
+    # load the mesh from filename
+    # file objects are also supported
+    mesh = trimesh.load_mesh('../models/featuretype.STL')
+
+    # split the mesh into connected components of the face adjacency graph. 
+    # splitting sometimes produces non- watertight meshes
+    # though the splitter will try to repair single quad and
+    # single triangle holes, in our case here we are going to be 
+    # taking convex hulls anyway, so there is no reason to not discard
+    # the non- watertight bodies
+    meshes = mesh.split(check_watertight=False)
+
+    # the convex hull of every component
+    meshes_convex = [i.convex_hull() for i in meshes]
+
+    # combine all components into one mesh
+    convex_combined = np.sum(meshes_convex)
+
+    # open a viewer window
+    convex_combined.show()
---
+++ examples/section.py
@@ -0,0 +1,40 @@
+'''
+examples/section.py
+
+Slice a mesh into 2D sections, like you would do to 
+write a basic 3D printing driver. 
+'''
+
+import trimesh
+import numpy as np
+
+if __name__ == '__main__':
+    # load the mesh from filename
+    # file objects are also supported
+    mesh = trimesh.load_mesh('../models/featuretype.STL')
+
+    # we're going to slice the mesh into evenly spaced chunks along z
+    # this takes the (2,3) bounding box and slices it into [minz, maxz]
+    z_extents = mesh.bounds[:,2]
+
+    # 10 evenly spaced z values, between the extents of the mesh
+    z_levels  = np.linspace(*z_extents, num=10)
+
+    # create an array to hold the section objects
+    sections  = [None] * len(z_levels)
+
+    for i, z in enumerate(z_levels):
+        # this will return a Path3D object, each of which will 
+        # have curves in 3D space
+        sections[i] = mesh.section(plane_origin = [0,0,z],
+                                   plane_normal = [0,0,1])
+
+    # summing the array of path objects will put all of the curves
+    # into one Path3D object, which we can then plot easily in 3D
+    np.sum(sections).show()
+
+    # we can also transform each section in space onto the XY plane
+    # note that if the Path3D object couldn't find a plane which all 
+    # of the entities lie on, it will raise a MeshError
+    section_2D, T_matrix = sections[0].to_planar()
+    section_2D.show()
