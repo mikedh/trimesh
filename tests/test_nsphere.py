054eea210755d9bd6c0f28c5fb7df35f458c4ffb
Michael Dawson-Haggerty <mik3dh@gmail.com>
2016-08-19 19:59:17 -0400

    added checks to nsphere tests

--- tests/test_nsphere.py
+++ tests/test_nsphere.py
@@ -3,18 +3,26 @@ import generic as g
 class NSphereTest(g.unittest.TestCase):
         
     def test_minball(self):
-        for m in g.get_meshes(5):
-            s = m.bounding_sphere
-            self.assertTrue(s.volume > (m.volume-g.tol.merge))
-            
-        s_degenerate = g.trimesh.primitives.Sphere().bounding_sphere
 
-        for d in [2,3]:
+        # get some assorted mesh geometries to test general performance
+        # and a perfect sphere mesh to test the degenerate case
+        for m in g.np.append(g.get_meshes(5),
+                             g.trimesh.primitives.Sphere()):
+
+            s = m.bounding_sphere
+            R_check = ((m.vertices - s.sphere_center)**2).sum(axis=1).max() ** .5
+            
+            self.assertTrue(len(s.sphere_center) == 3)
+            self.assertTrue(s.sphere_radius > 0.0)
+            self.assertTrue(abs(s.sphere_radius[0] - R_check) < g.tol.fit)
+            self.assertTrue(s.volume > (m.volume-g.tol.fit))
+
+        # check minimum n-sphere for sets of points in 2,3, and 4 dimensions
+        for d in [2,3,4]:
             for i in range(5):
                 points = g.np.random.random((100,d))
                 C,R = g.trimesh.nsphere.minimum_nsphere(points)
                 R_check = ((points - C)**2).sum(axis=1).max() ** .5
-
                 self.assertTrue(len(C) == d)
                 self.assertTrue(R > 0.0)
                 self.assertTrue(abs(R - R_check) < g.tol.merge)
