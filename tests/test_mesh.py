c7dd29e76ddcd780e143f439b6a15f1e63961bc4
Michael Dawson-Haggerty <mik3dh@gmail.com>
2016-11-17 21:48:08 -0500

    travis attempt

--- tests/test_mesh.py
+++ tests/test_mesh.py
@@ -76,8 +76,8 @@ class MeshTests(g.unittest.TestCase):
             self.assertTrue(hasattr(r, 'intersection'))
 
             # some memory issues only show up when you copy the mesh a bunch
-            for i in range(100):
-                #c = mesh.copy()
+            for i in range(10):
+                c = mesh.copy()
                         
     def test_fill_holes(self):
         for mesh in g.get_meshes(5):
