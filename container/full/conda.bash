fead57b9c55fb1fb6b8e9a97e1818a80085b2fb1
Michael Dawson-Haggerty <mik3dh@gmail.com>
2016-12-22 21:51:34 -0800

    added missing docker install scripts

---
+++ container/full/conda.bash
@@ -0,0 +1,14 @@
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+bash miniconda.sh -b -p $HOME/miniconda
+
+export PATH="$HOME/miniconda/bin:$PATH"
+
+conda config --set always_yes yes --set changeps1 no
+
+conda config --add channels ostrokach
+conda config --add channels conda-forge
+conda config --add channels scitools
+conda config --add channels ioos
+conda config --add channels defaults
+conda update -q conda
+conda install shapely rtree graph-tool
