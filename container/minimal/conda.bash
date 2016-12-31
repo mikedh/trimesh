0a5f5f9407889525e04b9a27f8c3c528860fc4a4
Michael Dawson-Haggerty <mik3dh@gmail.com>
2016-12-31 01:17:29 -0500

    moved all docker images to jessie

--- container/minimal/conda.bash
+++ container/minimal/conda.bash
@@ -12,17 +12,24 @@ conda update -q conda
 conda create -q -n docker-environment python=3.5
 source activate docker-environment
 
-conda config --add channels ostrokach    # graph-tool
+# add channels
+# graph-tool installs 1.8gb worth of stuff
+# however it is really useful if you are calling
+# mesh.split() on a lot of large meshes
+#conda config --add channels ostrokach   # graph-tool
 conda config --add channels conda-forge  # rtree, shapely, pyembree
 conda config --add channels menpo        # cyassimp
 conda config --add channels defaults     # stuff, things
+
+# use openBLAS instead of MKL (saves 400mb and is in some cases faster)
+conda install nomkl
 
 # cyassimp is a much faster binding for the assimp importers
 # they use non- standard labels, master vs main
 conda install -c menpo/label/master cyassimp
 
 # install most trimesh requirements with built components 
-conda install shapely rtree graph-tool pyembree numpy scipy
+conda install shapely rtree  pyembree numpy scipy #graph-tool
 
 # remove archives
 conda clean --all -y
