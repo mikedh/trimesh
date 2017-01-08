8d14c7fd133412c930dea6cc31bb48013fc00bfb
Michael Dawson-Haggerty <mik3dh@gmail.com>
2017-01-07 22:50:23 -0500

    better error message

--- tests/test_dxf.py
+++ tests/test_dxf.py
@@ -5,14 +5,18 @@ class DXFTest(g.unittest.TestCase):
         self.drawings = [g.trimesh.load_path(i) for i in g.data['2D_files']]
         self.single = g.np.hstack([i.split() for i in self.drawings])
 
-    def test_export(self):
+    def test_dxf(self):
         for p in self.single:
             p.vertices /= p.scale
             p.export(file_obj='temp.dxf')
             r = g.trimesh.load('temp.dxf')
             ratio = abs(p.area - r.area) / p.area
-            assert ratio < .001
-
+            if ratio > .001:
+                log.error('Area ratio on export wrong! %f %f',
+                          p.area,
+                          r.area)
+                raise ValueError('Area ratio too large')
+            
 if __name__ == '__main__':
     g.trimesh.util.attach_to_log()
     g.unittest.main()
