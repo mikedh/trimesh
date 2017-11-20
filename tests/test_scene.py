9534e841b2abcbf37393443f2eb0b39f4ecd475b
Michael Dawson-Haggerty <mik3dh@gmail.com>
2017-11-20 12:47:06 -0800

    test namespace typo

--- tests/test_scene.py
+++ tests/test_scene.py
@@ -63,7 +63,7 @@ class SceneTests(g.unittest.TestCase):
 
 
                 s.rezero()
-                assert (np.abs(s.centroid) < 1e-3).all()
+                assert (g.np.abs(s.centroid) < 1e-3).all()
 
 
     def test_scaling(self):
