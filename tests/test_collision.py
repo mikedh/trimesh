d574975a6e5a009df0a19ad31e016bfdbc3bb481
Matthew Matl <matthewcmatl@gmail.com>
2018-01-12 12:40:21 -0800

    Add distance checking wrappers to collision mgr
    This relies on python-fcl>=0.0.7 to work (there was a bug in 0.0.6
    for distance checking specifically).

--- tests/test_collision.py
+++ tests/test_collision.py
@@ -80,6 +80,88 @@ class CollisionTest(g.unittest.TestCase):
         self.assertTrue(('cube1', 'cube3') in names)
         self.assertTrue(('cube3', 'cube1') not in names)
 
+    def test_distance(self):
+        # Ensure that FCL is importable
+        try:
+            g.trimesh.collision.CollisionManager()
+        except ValueError:
+            g.log.warning('skipping collision tests, no FCL installed')
+            return
+
+        cube = g.get_mesh('unit_cube.STL')
+
+        tf1 = g.np.eye(4)
+        tf1[:3, 3] = g.np.array([5, 0, 0])
+
+        tf2 = g.np.eye(4)
+        tf2[:3, 3] = g.np.array([-5, 0, 0])
+
+        tf3 = g.np.eye(4)
+        tf3[:3, 3] = g.np.array([2, 0, 0])
+
+        tf4 = g.np.eye(4)
+        tf4[:3, 3] = g.np.array([-2, 0, 0])
+
+        # Test one-to-many distance checking
+        m = g.trimesh.collision.CollisionManager()
+        m.add_object('cube1', cube, tf1)
+
+        dist = m.min_distance_single(cube)
+        self.assertTrue(g.np.isclose(dist, 4.0))
+
+        dist, name = m.min_distance_single(cube, return_name=True)
+        self.assertTrue(g.np.isclose(dist, 4.0))
+        self.assertTrue(name == 'cube1')
+
+        m.add_object('cube2', cube, tf2)
+
+        dist, name = m.min_distance_single(cube, tf3, return_name=True)
+        self.assertTrue(g.np.isclose(dist, 2.0))
+        self.assertTrue(name == 'cube1')
+
+        dist, name = m.min_distance_single(cube, tf4, return_name=True)
+        self.assertTrue(g.np.isclose(dist, 2.0))
+        self.assertTrue(name == 'cube2')
+
+        # Test internal distance checking and object
+        # addition/removal/modification
+        dist = m.min_distance_internal()
+        self.assertTrue(g.np.isclose(dist, 9.0))
+
+        dist, names = m.min_distance_internal(return_names=True)
+        self.assertTrue(g.np.isclose(dist, 9.0))
+        self.assertTrue(names == ('cube1', 'cube2'))
+
+        m.add_object('cube3', cube, tf3)
+
+        dist, names = m.min_distance_internal(return_names=True)
+        self.assertTrue(g.np.isclose(dist, 2.0))
+        self.assertTrue(names == ('cube1', 'cube3'))
+
+        m.set_transform('cube3', tf4)
+
+        dist, names = m.min_distance_internal(return_names=True)
+        self.assertTrue(g.np.isclose(dist, 2.0))
+        self.assertTrue(names == ('cube2', 'cube3'))
+
+        # Test manager-to-manager distance checking
+        m = g.trimesh.collision.CollisionManager()
+        m.add_object('cube0', cube)
+        m.add_object('cube1', cube, tf1)
+
+        n = g.trimesh.collision.CollisionManager()
+        n.add_object('cube0', cube, tf2)
+
+        dist, names = m.min_distance_other(n, return_names=True)
+        self.assertTrue(g.np.isclose(dist, 4.0))
+        self.assertTrue(names == ('cube0', 'cube0'))
+
+        n.add_object('cube4', cube, tf4)
+
+        dist, names = m.min_distance_other(n, return_names=True)
+        self.assertTrue(g.np.isclose(dist, 1.0))
+        self.assertTrue(names == ('cube0', 'cube4'))
+
 
 if __name__ == '__main__':
     g.trimesh.util.attach_to_log()
