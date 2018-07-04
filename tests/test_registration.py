6619ec4340a8acb1cf0c311470673fee6b950c48
mikedh <mik3dh@gmail.com>
2018-07-04 18:20:32 -0400

    ICP fixes and version bump

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,27 +1,64 @@
-import generic as g
+try:
+    from . import generic as g
+except BaseException:
+    import generic as g
+
 
 class RegistrationTest(g.unittest.TestCase):
 
     def test_procrustes(self):
-        
-        X = g.np.random.normal(size=30).reshape(-1,3)
-        Y = g.trimesh.transformations.transform_points(
-                X, g.trimesh.transformations.random_rotation_matrix())
-        Y = g.np.random.uniform(1,100)*Y + 100*g.np.random.normal(size=3)
-        
-        matrix, transformed, cost = g.trimesh.registration.procrustes(X, Y)
-        assert(cost < 0.01)
+        # create random points in space
+        points_a = (g.np.random.random((1000, 3)) - .5) * 1000
+        # create a random transform
+        matrix = g.trimesh.transformations.random_rotation_matrix()
+        # add a translation component to transform
+        matrix[:3, 3] = g.np.random.random(3) * 100
+        # apply transform to points A
+        points_b = g.trimesh.transform_points(points_a, matrix)
 
-    def test_icp(self):
+        # run the solver
+        (matrixN,
+         transformed,
+         cost) = g.trimesh.registration.procrustes(points_a, points_b)
+        # the points should be identical
+        assert(cost < 0.01)
+        # it should have found the matrix we used
+        assert g.np.allclose(matrixN, matrix)
+
+    def test_icp_mesh(self):
+        # see if ICP alignment works with meshes
         m = g.trimesh.creation.box()
         X = m.sample(10)
         X = X + [0.1, 0.1, 0.1]
-        
-        matrix, transformed, cost = g.trimesh.registration.icp(X, m, scale=False)
-        
+        matrix, transformed, cost = g.trimesh.registration.icp(
+            X, m, scale=False)
         assert(cost < 0.01)
 
-        
+    def test_icp_points(self):
+        # see if ICP alignment works with point clouds
+        # create random points in space
+        points_a = (g.np.random.random((1000, 3)) - .5) * 1000
+        # create a random transform
+        #matrix = g.trimesh.transformations.random_rotation_matrix()
+        # create a small transform
+        # ICP will not work at all with large transforms
+        matrix = g.trimesh.transformations.rotation_matrix(
+            g.np.radians(1.0),
+            [0, 0, 1])
+
+        # take a few randomly chosen points and make
+        # sure the order is permutated
+        index = g.np.random.choice(g.np.arange(len(points_a)), 20)
+        # transform and apply index
+        points_b = g.trimesh.transform_points(points_a[index], matrix)
+        # tun the solver
+        matrixN, transformed, cost = g.trimesh.registration.icp(points_b,
+                                                                points_a)
+        assert cost < 1e-3
+        assert g.np.allclose(matrix,
+                             g.np.linalg.inv(matrixN))
+        assert g.np.allclose(transformed, points_a[index])
+
 if __name__ == '__main__':
     g.trimesh.util.attach_to_log()
     g.unittest.main()
--- trimesh/registration.py
+++ trimesh/registration.py
@@ -5,16 +5,23 @@ registration.py
 Functions for registering (aligning) point clouds with meshes.
 """
 
-from . import base
+import numpy as np
+
+from scipy.spatial import cKDTree
+
 from . import util
 from .transformations import transform_points
-import numpy as np
-from scipy.spatial import KDTree  
 
-def procrustes(a, b, reflection=True, translation=True, scale=True, return_cost=True):
+
+def procrustes(a,
+               b,
+               reflection=True,
+               translation=True,
+               scale=True,
+               return_cost=True):
     """
-    Perform Procrustes' analysis subject to constraints. Finds the 
-    transformation T mapping a to b which minimizes the sums-of-squares 
+    Perform Procrustes' analysis subject to constraints. Finds the
+    transformation T mapping a to b which minimizes the sums-of-squares
     distances between Ta and b, also called the cost.
 
     Parameters
@@ -25,7 +32,7 @@ def procrustes(a, b, reflection=True, translation=True, scale=True, return_cost=
     translation : bool, if the transformation is allowed translations
     scale       : bool, if the transformation is allowed scaling
     return_cost : bool, whether to return the cost and transformed a as well
-    
+
     Returns
     ----------
     matrix      : (4,4) float, the transformation matrix sending a to b
@@ -37,10 +44,10 @@ def procrustes(a, b, reflection=True, translation=True, scale=True, return_cost=
     b = np.asanyarray(b, dtype=np.float64)
     if not util.is_shape(a, (-1, 3)) or not util.is_shape(b, (-1, 3)):
         raise ValueError('points must be (n,3)!')
-        
+
     if len(a) != len(b):
         raise ValueError('a and b must contain same number of points!')
-        
+
     # Remove translation component
     if translation:
         acenter = a.mean(axis=0)
@@ -48,42 +55,49 @@ def procrustes(a, b, reflection=True, translation=True, scale=True, return_cost=
     else:
         acenter = np.zeros(a.shape[1])
         bcenter = np.zeros(b.shape[1])
-        
+
     # Remove scale component
     if scale:
-        ascale = np.sqrt(((a-acenter)**2).sum()/len(a))
-        bscale = np.sqrt(((b-bcenter)**2).sum()/len(b))
+        ascale = np.sqrt(((a - acenter)**2).sum() / len(a))
+        bscale = np.sqrt(((b - bcenter)**2).sum() / len(b))
     else:
         ascale = 1
         bscale = 1
-        
+
     # Use SVD to find optimal orthogonal matrix R
     # constrained to det(R) = 1 if necessary.
-    u, s, vh = np.linalg.svd(np.dot(((b-bcenter)/bscale).T, ((a-acenter)/ascale)))
+    u, s, vh = np.linalg.svd(
+        np.dot(((b - bcenter) / bscale).T, ((a - acenter) / ascale)))
     if reflection:
         R = np.dot(u, vh)
     else:
-        R = np.dot(np.dot(u, np.diag([1, 1, np.linalg.det(np.dot(u, vh))])), vh)
-        
+        R = np.dot(np.dot(u, np.diag(
+            [1, 1, np.linalg.det(np.dot(u, vh))])), vh)
+
     # Compute our 4D transformation matrix encoding
     # a -> (R @ (a - acenter)/ascale) * bscale + bcenter
     #    = (bscale/ascale)R @ a + (bcenter - (bscale/ascale)R @ acenter)
-    translation = bcenter - (bscale/ascale) * np.dot(R, acenter)
-    matrix = np.hstack((bscale/ascale * R, translation.reshape(-1, 1)))
-    matrix = np.vstack((matrix, np.array([0.]*(a.shape[1]) + [1.]).reshape(1, -1)))
-    
+    translation = bcenter - (bscale / ascale) * np.dot(R, acenter)
+    matrix = np.hstack((bscale / ascale * R, translation.reshape(-1, 1)))
+    matrix = np.vstack(
+        (matrix, np.array([0.] * (a.shape[1]) + [1.]).reshape(1, -1)))
+
     if return_cost:
         transformed = transform_points(a, matrix)
         cost = ((b - transformed)**2).mean()
         return matrix, transformed, cost
     else:
         return matrix
-    
-def icp(a, b, initial=np.identity(4), 
-        threshold=1e-5, max_iterations=20, **kwargs):
 
+
+def icp(a,
+        b,
+        initial=np.identity(4),
+        threshold=1e-5,
+        max_iterations=20,
+        **kwargs):
     """
-    Apply the iterative closest point algorithm to align a point cloud with 
+    Apply the iterative closest point algorithm to align a point cloud with
     another point cloud or mesh. Will only produce reasonable results if the
     initial transformation is roughly correct. Initial transformation can be
     found by applying Procrustes' analysis to a suitable set of landmark
@@ -97,54 +111,51 @@ def icp(a, b, initial=np.identity(4),
     threshold      : float, stop when change in cost is less than threshold
     max_iterations : int, maximum number of iterations
     kwargs         : dict, args to pass to procrustes
-    
+
     Returns
     ----------
     matrix      : (4,4) float, the transformation matrix sending a to b
     transformed : (n,3) float, the image of a under the transformation
     cost        : float, the cost of the transformation
     """
-    
+
     a = np.asanyarray(a, dtype=np.float64)
     if not util.is_shape(a, (-1, 3)):
         raise ValueError('points must be (n,3)!')
-                
-    is_mesh = isinstance(b, base.Trimesh)
+
+    is_mesh = util.is_instance_named(b, 'Trimesh')
     if not is_mesh:
         b = np.asanyarray(b, dtype=np.float64)
         if not util.is_shape(b, (-1, 3)):
             raise ValueError('points must be (n,3)!')
-        btree = KDTree(b)
+        btree = cKDTree(b)
 
     # Transform a under initial_transformation
-    a = transform_points(a, initial) 
+    a = transform_points(a, initial)
     total_matrix = initial
-    
-    n_iteration = 0
+
+    # start with infinite cost
     old_cost = np.inf
-    while n_iteration < max_iterations:
-        n_iteration += 1
-        
+
+    # avoid looping forever by capping iterations
+    for n_iteration in range(max_iterations):
         # Closest point in b to each point in a
         if is_mesh:
             closest, distance, faces = b.nearest.on_surface(a)
         else:
             distances, ix = btree.query(a, 1)
             closest = b[ix]
-        
+
         # Align a with closest points
         matrix, transformed, cost = procrustes(a, closest, **kwargs)
 
-        # Update a 
+        # Update a
         a = transformed
-        total_matrix =  np.dot(matrix, total_matrix)
-        
+        total_matrix = np.dot(matrix, total_matrix)
+
         if old_cost - cost < threshold:
             break
         else:
             old_cost = cost
-         
+
     return total_matrix, transformed, cost
-            
-        
-        
