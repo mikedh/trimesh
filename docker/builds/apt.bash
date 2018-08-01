1cdbfdb6856992151e50bbfa133b6881c2493be1
Michael Dawson-Haggerty <mik3dh@gmail.com>
2018-08-01 10:49:52 -0400

    docker fix wget args

--- docker/builds/apt.bash
+++ docker/builds/apt.bash
@@ -2,7 +2,8 @@ set -xe
 apt-get update
 apt-get install -y --no-install-recommends blender openscad wget bzip2 supervisor libgl1-mesa-glx libgl1-mesa-dri xvfb libgeos-dev libspatialindex-c4v5
 
-wget https://download.opendesign.com/guestfiles/TeighaFileConverter/TeighaFileConverter_QT5_lnxX64_4.7dll.deb teigha.deb
+# get teigha converter for DWG to DXF conversion
+wget https://download.opendesign.com/guestfiles/TeighaFileConverter/TeighaFileConverter_QT5_lnxX64_4.7dll.deb --no-check-certificate --quiet -O teigha.deb
 dpkg -i teigha.deb
 rm teigha.deb
 
