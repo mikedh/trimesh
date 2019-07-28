0b4a8a9fc4ccb27bb3d5d74c31911ac9c85349b4
Dennis Bukenberger <dennis.bukenberger@uni-tuebingen.de>
2019-07-28 18:32:47 +0200

    Tests for the new vertex normal computation method added with a ground-truth reference model, generated in MeshLab.

--- tests/test_normals.py
+++ tests/test_normals.py
@@ -34,6 +34,57 @@ class NormalsTest(g.unittest.TestCase):
         assert mesh.vertex_normals.shape == mesh.vertices.shape
         assert g.np.allclose(mesh.vertex_normals - truth, 0.0)
 
+    def test_weighted_vertex_normals(self):
+
+        def compare_trimesh_to_groundtruth(mesh, truth, atol = g.trimesh.tol.merge):
+            # force fallback to loop normal summing by passing None
+            # as the sparse matrix
+            normals = g.trimesh.geometry.weighted_vertex_normals(
+                len(mesh.vertices),
+                mesh.faces,
+                mesh.face_normals,
+                mesh.face_angles,
+                sparse=None)
+            assert g.np.allclose(normals - truth, 0.0, atol = atol)
+
+            # make sure the automatic sparse matrix generation works
+            normals = g.trimesh.geometry.weighted_vertex_normals(
+                len(mesh.vertices),
+                mesh.faces,
+                mesh.face_normals,
+                mesh.face_angles)
+            assert g.np.allclose(normals - truth, 0.0, atol = atol)
+
+            # make sure the Trimesh normals- related attributes
+            # are wired correctly
+            assert mesh.faces_sparse is not None
+            assert mesh.vertex_normals.shape == mesh.vertices.shape
+            assert g.np.allclose(mesh.vertex_normals - truth, 0.0, atol = atol)
+
+        # the icosahedron is centered at zero, so the true vertex
+        # normal is just a unit vector of the vertex position
+        ico_mesh = g.trimesh.creation.icosahedron()
+        ico_truth = g.trimesh.util.unitize(ico_mesh.vertices)
+        compare_trimesh_to_groundtruth(ico_mesh, ico_truth)
+
+        # create a cube centered at zero, as with the icosahedron,
+        # normals compute as unit vectors of the corner vertices
+        # due to the triangulation of tbe box, this case would fail
+        # with a simple face-normals-average as vertex-normal method
+        box_mesh = g.trimesh.creation.box()
+        box_truth = g.trimesh.util.unitize(box_mesh.vertices)
+        compare_trimesh_to_groundtruth(box_mesh, box_truth)
+
+        # load the fandisk model: a complex triangulated mesh with
+        # smooth curved surfaces, corners and sharp creases
+        # ground truth vertex normals were computed in MeshLab and
+        # are included in the file
+        fandisk_mesh = g.get_mesh('fandisk.obj')
+        fandisk_truth = fandisk_mesh.vertex_normals
+        # due to the limited precision in the MeshLab export,
+        # we have to tweak the tolerance for the comparison a little
+        compare_trimesh_to_groundtruth(fandisk_mesh, fandisk_truth, 0.0001)
+
     def test_face_normals(self):
         """
         Test automatic generation of face normals on mesh objects
