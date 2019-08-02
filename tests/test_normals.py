fix column calculation

tests/test_normals.py
@@ -102,7 +102,7 @@ class NormalsTest(g.unittest.TestCase):
             faces=m.faces,
             face_normals=m.face_normals,
             face_angles=m.face_angles,
-            sparse=10)
+            use_loop=True)
         assert g.np.isfinite(norm).all()
         assert len(norm) == len(m.vertices)
 

trimesh/exchange/obj.py
@@ -120,12 +120,12 @@ def load_obj(file_obj,
         # also wavefront is 1- indexed (vs 0- indexed) so offset
         array = np.fromstring(joined, sep=' ', dtype=np.int64) - 1
 
-        # get the number of columns in a sample line
-        columns = len(face_lines[0].strip().split())
+        # get the number of raw 2D columns in a sample line
+        columns = len(face_lines[0].strip().replace('/', ' ').split())
 
-        # make sure we have the right number of values
+        # make sure we have the right number of values for vectorized
         if len(array) == (columns * len(face_lines)):
-            # everything is a nice even 2D array
+            # everything is a nice 2D array
             faces, faces_tex, faces_norm = _parse_faces_vectorized(
                 array=array,
                 columns=columns,
@@ -136,7 +136,8 @@ def load_obj(file_obj,
             # i.e. something like:
             #  '31407 31406 31408',
             #  '32303/2469 32304/2469 32305/2469',
-            log.warning('faces have mixed data, using slow fallback!')
+            log.warning('faces have mixed data, using slow fallback!',
+                        len(array), columns, len(face_lines))
             faces, faces_tex, faces_norm = _parse_faces_fallback(face_lines)
 
         # TODO: this usually falls back to something useless

trimesh/geometry.py
@@ -322,7 +322,7 @@ def mean_vertex_normals(vertex_count,
         summed = summed_sparse()
     except BaseException:
         log.warning(
-            'unable to generate sparse matrix! Falling back!',
+            'unable to use sparse matrix, falling back!',
             exc_info=True)
         summed = summed_loop()
 
@@ -336,7 +336,7 @@ def weighted_vertex_normals(vertex_count,
                             faces,
                             face_normals,
                             face_angles,
-                            sparse=None):
+                            use_loop=False):
     """
     Compute vertex normals from the faces that contain that vertex.
     The contibution of a face's normal to a vertex normal is the
@@ -371,12 +371,10 @@ def weighted_vertex_normals(vertex_count,
         # fill the matrix with vertex-corner angles as weights
         corner_angles = face_angles[np.repeat(np.arange(len(faces)), 3),
                                     np.argsort(faces, axis=1).ravel()]
-        if sparse is None:
-            matrix = index_sparse(vertex_count, faces).astype(np.float64)
-            matrix.data = corner_angles
-        else:
-            matrix = sparse.copy().astype(np.float64)
-            matrix.data = corner_angles
+        # create a sparse matrix
+        matrix = index_sparse(vertex_count, faces).astype(np.float64)
+        # assign the corner angles to the sparse matrix data
+        matrix.data = corner_angles
 
         return matrix.dot(face_normals)
 
@@ -402,13 +400,15 @@ def weighted_vertex_normals(vertex_count,
     face_normals = face_normals[face_ok]
     face_angles = face_angles[face_ok]
 
-    try:
-        return util.unitize(summed_sparse())
-    except BaseException:
-        log.warning(
-            'unable to generate sparse matrix! Falling back!',
-            exc_info=True)
-        return util.unitize(summed_loop())
+    if not use_loop:
+        try:
+            return util.unitize(summed_sparse())
+        except BaseException:
+            log.warning(
+                'unable to use sparse matrix, falling back!',
+                exc_info=True)
+    # we either crashed or were asked to loop
+    return util.unitize(summed_loop())
 
 
 def index_sparse(columns, indices, data=None):