dfcb9a97ec8dec7526af6d5d24c02dddbd09a1ad
Michael Dawson-Haggerty <mikedh@kerfed.com>
2020-02-29 16:40:24 -0500

    ECR check

--- docker/builds/apt.bash
+++ docker/builds/apt.bash
@@ -1,6 +1,6 @@
 set -xe
+
 apt-get update -qq
-apt-get upgrade -y -qq --no-install-recommends
 apt-get install -y -qq --no-install-recommends wget bzip2 supervisor \
 	libgl1-mesa-glx libgl1-mesa-dri xvfb xauth \
 	libassimp-dev ca-certificates zstd unzip \
