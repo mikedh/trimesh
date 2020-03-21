c4310c958cc713f621eca97f52d0cf04e369d922
mikedh <mik3dh@gmail.com>
2020-03-21 19:41:37 -0400

    fix docker typo and gate releases on docker builds

--- docker/builds/apt.bash
+++ docker/builds/apt.bash
@@ -11,7 +11,7 @@ apt-get install -y -qq --no-install-recommends \
 # install newer version of pandoc
 wget https://github.com/jgm/pandoc/releases/download/2.9.2/pandoc-2.9.2-1-amd64.deb
 echo "78525735ac6181f639c5c8776572d0ca10f0314c0052f5af2f369b5d0e1980b3  pandoc-2.9.2-1-amd64.deb" | sha256sum --check
-sudo dpkg install 
+sudo dpkg --install pandoc-2.9.2-1-amd64.deb
 rm -f pandoc-2.9.2-1-amd64.deb
 
 # remove garbage
