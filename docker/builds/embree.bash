f8b35048f6e5b9deb7c10f623897b41105ac4b96
Pras Velagapudi <psigen@gmail.com>
2020-04-12 16:29:06 -0400

    Removed unnecessary print.

--- docker/builds/embree.bash
+++ docker/builds/embree.bash
@@ -2,7 +2,6 @@
 
 # Fetch the archive from GitHub releases.
 wget https://github.com/embree/embree/releases/download/v2.17.7/embree-2.17.7.x86_64.linux.tar.gz -O /tmp/embree.tar.gz -nv
-sha256sum /tmp/embree.tar.gz
 echo "a3ac692397574166ad99493ff1efcd7b5c69b580e7eb4500d5d181b2f676aa6e  /tmp/embree.tar.gz" | sha256sum --check
 tar -xzf /tmp/embree.tar.gz --strip-components=1 -C /usr/local
 
