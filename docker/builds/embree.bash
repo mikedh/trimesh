2f8a107e199872850442fe35edcba504ba0f0dc5
mikedh <mik3dh@gmail.com>
2020-04-12 19:57:27 -0400

    remove archive

--- docker/builds/embree.bash
+++ docker/builds/embree.bash
@@ -5,6 +5,8 @@ set -xe
 wget https://github.com/embree/embree/releases/download/v2.17.7/embree-2.17.7.x86_64.linux.tar.gz -O /tmp/embree.tar.gz -nv
 echo "2c4bdacd8f3c3480991b99e85b8f584975ac181373a75f3e9675bf7efae501fe  /tmp/embree.tar.gz" | sha256sum --check
 tar -xzf /tmp/embree.tar.gz --strip-components=1 -C /usr/local
+# remove archive
+rm -rf /tmp/embree.tar.gz
 
 # Install python bindings for embree (and upstream requirements).
 pip install --no-cache-dir numpy cython
