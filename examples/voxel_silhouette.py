00cb1c718387666926a1ee750d4dcf31af15e13f
mikedh <mik3dh@gmail.com>
2020-04-19 15:54:16 -0400

    fix voxel_silhouette example

--- examples/voxel_silhouette.py
+++ examples/voxel_silhouette.py
@@ -4,41 +4,6 @@ import numpy as np
 from PIL import Image
 
 
-resolution = 256
-fov = 60.
-path = os.path.realpath(
-    os.path.join(os.path.dirname(__file__), '..', 'models', 'bunny.ply'))
-
-mesh = trimesh.load(path)
-scene = mesh.scene()
-camera = scene.camera
-
-camera.fov = (fov,) * 2
-camera.resolution = (resolution,) * 2
-origin, rays = scene.camera_rays()
-rays = rays.reshape((resolution, resolution, 3))
-offset = mesh.vertices - origin
-
-# dists is vertices projected onto central ray
-dists = np.dot(offset, rays[rays.shape[0] // 2, rays.shape[1] // 2])
-closest = np.min(dists)
-farthest = np.max(dists)
-z = np.linspace(closest, farthest, resolution)
-print('z range: %f, %f' % (closest, farthest))
-
-vox = mesh.voxelized(1. / resolution, method='binvox')
-
-coords = np.expand_dims(rays, axis=-2) * np.expand_dims(z, axis=-1)
-coords += origin
-frust_vox_dense = vox.is_filled(coords)
-sil = np.any(frust_vox_dense, axis=-1)
-sil = sil.T  # change to image ordering (y, x)
-
-image = np.array(Image.open(trimesh.util.wrap_as_stream(
-    scene.save_image(resolution=None))))
-image = image[..., :3]
-
-
 def vis():
     # separate function to delay plt import
     import matplotlib.pyplot as plt
@@ -51,4 +16,42 @@ def vis():
     plt.show()
 
 
-vis()
+if __name__ == '__main__':
+
+    resolution = 256
+    fov = 60.0
+    path = os.path.realpath(
+        os.path.join(os.path.dirname(__file__), '..', 'models', 'bunny.ply'))
+
+    mesh = trimesh.load(path)
+    scene = mesh.scene()
+    camera = scene.camera
+
+    camera.fov = (fov,) * 2
+    camera.resolution = (resolution,) * 2
+
+    origins, rays, px = scene.camera_rays()
+    origin = origins[0]
+    rays = rays.reshape((resolution, resolution, 3))
+    offset = mesh.vertices - origin
+
+    # dists is vertices projected onto central ray
+    dists = np.dot(offset, rays[rays.shape[0] // 2, rays.shape[1] // 2])
+    closest = np.min(dists)
+    farthest = np.max(dists)
+    z = np.linspace(closest, farthest, resolution)
+    print('z range: %f, %f' % (closest, farthest))
+
+    vox = mesh.voxelized(1. / resolution, method='binvox')
+
+    coords = np.expand_dims(rays, axis=-2) * np.expand_dims(z, axis=-1)
+    coords += origin
+    frust_vox_dense = vox.is_filled(coords)
+    sil = np.any(frust_vox_dense, axis=-1)
+    sil = sil.T  # change to image ordering (y, x)
+
+    image = np.array(Image.open(trimesh.util.wrap_as_stream(
+        scene.save_image(resolution=None))))
+    image = image[..., :3]
+
+    vis()
