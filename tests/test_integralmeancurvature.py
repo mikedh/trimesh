7cb77cc2aaafa9b868c3dd6247438df7b3dfd7b7
Keisuke Ishihara <tidio634@gmail.com>
2021-08-16 18:25:04 +0200

    one more autopep8

--- tests/test_integralmeancurvature.py
+++ tests/test_integralmeancurvature.py
@@ -12,7 +12,7 @@ class IntegralMeanCurvatureTest(g.unittest.TestCase):
         for radius in [0.1, 1.0, 3.1459, 29.20]:
             m = g.trimesh.creation.icosphere(subdivisions=4, radius=radius)
             IMC = m.integral_mean_curvature
-            ref = 4*g.np.pi*radius
+            ref = 4 * g.np.pi * radius
             assert g.np.isclose(IMC, ref, rtol=tol)
 
     def test_IMCcapsule(self):
@@ -20,7 +20,7 @@ class IntegralMeanCurvatureTest(g.unittest.TestCase):
         tol = 1e-3
         radius = 1.2
         for aspect_ratio in [0.0, 0.5, 1.0, 4.0, 100]:
-            L = aspect_ratio*radius
+            L = aspect_ratio * radius
             m = g.trimesh.creation.capsule(height=L, radius=radius, count=[64, 64])
             IMC = m.integral_mean_curvature
             ref = g.np.pi * (L + 4 * radius)
@@ -30,13 +30,13 @@ class IntegralMeanCurvatureTest(g.unittest.TestCase):
         # how close do we need to be - relative tolerance
         tol = 1e-3
         n_tests = 4
-        extents = 1-g.np.random.random((n_tests, 3))
+        extents = 1 - g.np.random.random((n_tests, 3))
         for extent in extents:
             m = g.trimesh.creation.box(extents=extent)
             IMC = m.integral_mean_curvature
             # only the right angle (=pi/2) edges contribute,
             # edges that lie on the faces of the box do not
-            ref = 4 * extent.sum() * g.np.pi/2 * 0.5
+            ref = 4 * extent.sum() * g.np.pi / 2 * 0.5
             assert g.np.isclose(IMC, ref, rtol=tol)
 
 
