272d11c39a0e5914aee290d59d650c7d52dbe6f6
Clemens Eppner <ceppner@nvidia.com>
2022-02-08 09:29:20 -0800

    Deal with None as well

--- trimesh/exchange/obj.py
+++ trimesh/exchange/obj.py
@@ -849,7 +849,7 @@ def export_obj(mesh,
                 log.debug('failed to convert vertex normals',
                           exc_info=True)
 
-        if include_texture and hasattr(mesh.visual, 'uv') and mesh.visual.uv.size > 0:
+        if include_texture and hasattr(mesh.visual, 'uv') and len(np.shape(getattr(mesh.visual, 'uv', None))) == 2:
             try:
                 material = mesh.visual.material
                 if hasattr(material, 'to_simple'):
