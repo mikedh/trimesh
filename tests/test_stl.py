4ba0a99a626e54cd7ca68692c5135bcd6b2f8d3a
max <max@thousandyardstare.de>
2022-04-02 11:45:13 +0200

    Add test for STL vertex order

--- tests/test_stl.py
+++ tests/test_stl.py
@@ -89,6 +89,25 @@ class STLTests(g.unittest.TestCase):
             except BaseException:
                 return
             raise ValueError("Shouldn't export empty scenes!")
+    
+    def test_vertex_order(self):
+        # removing doubles should respect the vertex order
+        m_raw = g.get_mesh('featuretype.STL', process=False)
+        m_proc = g.get_mesh('featuretype.STL', process=True)
+
+        verts_raw = g.trimesh.grouping.hashable_rows(m_raw.vertices)
+        verts_proc = g.trimesh.grouping.hashable_rows(m_proc.vertices)        
+
+        # go through all processed verts
+        # find index in unprocessed mesh
+        idxs = []
+        for vert in verts_proc:
+            idxs.append(g.np.where(verts_raw == vert)[0][0])
+
+        # indices should be increasing
+        assert (g.np.diff(idxs) >= 0).all()
+
+
 
 
 if __name__ == '__main__':
