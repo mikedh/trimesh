bdd40851d2bb0ba818e19b43ff2b759c564e6b0f
Michael Dawson-Haggerty <mikedh@kerfed.com>
2022-05-02 12:58:21 -0400

    add ray shape error

--- trimesh/ray/ray_pyembree.py
+++ trimesh/ray/ray_pyembree.py
@@ -144,11 +144,13 @@ class RayMeshIntersector(object):
           Intersection points, only returned if return_locations
         """
         # make sure input is _dtype for embree
-        ray_origins = np.asanyarray(
+        ray_origins = np.array(
             deepcopy(ray_origins),
             dtype=np.float64)
         ray_directions = np.asanyarray(ray_directions,
                                        dtype=np.float64)
+        if ray_origins.shape != ray_directions.shape:
+            raise ValueError('Ray origin and direction don\'t match!')
         ray_directions = util.unitize(ray_directions)
 
         # since we are constructing all hits, save them to a deque then
@@ -178,10 +180,10 @@ class RayMeshIntersector(object):
             # run the pyembree query
             # if you set output=1 it will calculate distance along
             # ray, which is bizzarely slower than our calculation
+
             query = self._scene.run(
                 ray_origins[current],
                 ray_directions[current])
-
             # basically we need to reduce the rays to the ones that hit
             # something
             hit = query != -1
