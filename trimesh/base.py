be6c5b5a8375ccd38c5acec1481c7d65a8097755
Hyeonseo Nam <auspice7g@gmail.com>
2022-11-15 20:35:49 +0900

    edit loop subdivision for multibody case

--- trimesh/base.py
+++ trimesh/base.py
@@ -1997,7 +1997,7 @@ class Trimesh(Geometry3D):
 
         return result
       
-    def loop(self, iterations=1):
+    def loop(self, iterations=1, multibody=False):
         """
         Subdivide a mesh by dividing each triangle into four triangles 
         and approximating their smoothed surface (loop subdivision).
@@ -2006,16 +2006,38 @@ class Trimesh(Geometry3D):
         ------------
         iterations : int
           Number of iterations to run subdivisio
-        """
-        vertices = self.vertices
-        faces = self.faces
-        for _index in range(iterations):
-          vertices, faces = remesh.loop(vertices, faces)
+        multibody : bool
+          If True will try to subdivide for each submesh
+        """ 
+        if multibody:
+          splited_meshes = self.split(only_watertight=False)    
+          if len(splited_meshes) > 1:             
+            new_meshes = []
+            # perform subdivision for all submesh
+            for splited_mesh in splited_meshes:
+                new_vertices, new_faces = remesh.loop(
+                    vertices=splited_mesh.vertices, 
+                    faces=splited_mesh.faces, 
+                    iterations=iterations)
+                # create new mesh
+                new_mesh = Trimesh(
+                    vertices=new_vertices,
+                    faces=new_faces)
+                new_meshes.append(new_mesh)
+            # concatenate all meshes into one
+            result = util.concatenate(new_meshes)
+            return result
+          
+        # perform subdivision for one mesh
+        new_vertices, new_faces = remesh.loop(
+            vertices=self.vertices, 
+            faces=self.faces, 
+            iterations=iterations)
+        # create new mesh
         result = Trimesh(
-          vertices=vertices,
-          faces=faces)
+            vertices=new_vertices,
+            faces=new_faces)
         return result
-        
 
     @log_time
     def smoothed(self, **kwargs):
--- trimesh/remesh.py
+++ trimesh/remesh.py
@@ -217,7 +217,8 @@ def subdivide_to_size(vertices,
 
 
 def loop(vertices,
-         faces):
+         faces,
+         iterations=1):
     """
     Subdivide a mesh by dividing each triangle into four triangles 
     and approximating their smoothed surface (loop subdivision).
@@ -261,86 +262,93 @@ def loop(vertices,
       Vertices in space
     faces : (q, 3) int
       Indices of vertices
+    iterations : int
+          Number of iterations to run subdivision
     """
-    # find the unique edges of our faces 
-    edges, edges_face = faces_to_edges(faces, return_index=True)
-    edges = np.sort(edges, axis=1)
-    unique, inverse = grouping.unique_rows(edges)
+    def _subdivide(vertices, faces): 
+      # find the unique edges of our faces 
+      edges, edges_face = faces_to_edges(faces, return_index=True)
+      edges = np.sort(edges, axis=1)
+      unique, inverse = grouping.unique_rows(edges)
 
-    # set interior edges if there are two edges and boundary if there is one.
-    edge_inter = np.sort(grouping.group_rows(edges, require_count=2), axis=1)
-    edge_bound = grouping.group_rows(edges, require_count=1)
-    # make sure that one edge is shared by only one or two faces.
-    assert len(edge_inter) * 2 + len(edge_bound) == len(edges)
+      # set interior edges if there are two edges and boundary if there is one.
+      edge_inter = np.sort(grouping.group_rows(edges, require_count=2), axis=1)
+      edge_bound = grouping.group_rows(edges, require_count=1)
+      # make sure that one edge is shared by only one or two faces.
+      assert len(edge_inter) * 2 + len(edge_bound) == len(edges)
+      
+      # set interior, boundary mask for unique edges
+      edge_bound_mask = np.zeros(len(edges), dtype=bool)
+      edge_bound_mask[edge_bound] = True 
+      edge_bound_mask = edge_bound_mask[unique]
+      edge_inter_mask = ~edge_bound_mask
+      
+      # find the opposite face for each edge
+      edge_pair = np.zeros(len(edges)).astype(int)
+      edge_pair[edge_inter[:, 0]] = edge_inter[:, 1]
+      edge_pair[edge_inter[:, 1]] = edge_inter[:, 0]
+      opposite_face1 = edges_face[unique]
+      opposite_face2 = edges_face[edge_pair[unique]]
+
+      # set odd vertices to the middle of each edge (default as boundary case).
+      odd = vertices[edges[unique]].mean(axis=1)
+      # modify the odd vertices for the interior case
+      e = edges[unique[edge_inter_mask]] 
+      e_v0 = vertices[e][:, 0]
+      e_v1 = vertices[e][:, 1]
+      e_f0 = faces[opposite_face1[edge_inter_mask]]
+      e_f1 = faces[opposite_face2[edge_inter_mask]]
+      e_v2_idx = e_f0[~(e_f0[:, :, None] == e[:, None, :]).any(-1)]
+      e_v3_idx = e_f1[~(e_f1[:, :, None] == e[:, None, :]).any(-1)]
+      e_v2 = vertices[e_v2_idx]
+      e_v3 = vertices[e_v3_idx]
+      odd[edge_inter_mask] = 3/8 * (e_v0 + e_v1) + 1/8 * (e_v2 + e_v3)
+
+      # find vertex neighbors of each vertex 
+      neighbors = graph.neighbors(edges=edges[unique], max_index=len(vertices))
+      # convert list type of array into a fixed-shaped numpy array (set -1 to empties)
+      neighbors = np.array(list(zip_longest(*neighbors, fillvalue=-1))).T
+      # if the neighbor has -1 index, its point is (0, 0, 0), so that
+      # it is not included in the summation of neighbors when calculating the even values
+      vertices_ = np.vstack([vertices, [0, 0, 0]])
+      # number of neighbors
+      k = (neighbors + 1).astype(bool).sum(-1)
+      
+      # calculate even vertices for the interior case
+      even = np.zeros_like(vertices)
+      beta = 1/k * (5/8 - (3/8 + 1/4 * np.cos(2 * np.pi / k)) ** 2)
+      even = beta[:, None] * vertices_[neighbors].sum(1) + (1 - k[:, None] * beta[:, None]) * vertices
+
+      # calculate even vertices for the boundary case
+      if True in edge_bound_mask: 
+        # boundary vertices from boundary edges
+        vrt_bound_mask = np.zeros(len(vertices), dtype=bool)
+        vrt_bound_mask[np.unique(edges[unique][~edge_inter_mask])] = True 
+        # one boundary vertex has two neighbor boundary vertices (set others as -1)
+        boundary_neighbors = neighbors[vrt_bound_mask] 
+        boundary_neighbors[~vrt_bound_mask[neighbors[vrt_bound_mask]]] = -1
+        even[vrt_bound_mask] = 1/8 * vertices_[boundary_neighbors].sum(1) + 3/4 * vertices[vrt_bound_mask]
+
+      # the new faces with odd vertices
+      odd_idx = inverse.reshape((-1, 3)) + len(vertices)
+      new_faces = np.column_stack([faces[:, 0],
+                          odd_idx[:, 0],
+                          odd_idx[:, 2],
+                          odd_idx[:, 0],
+                          faces[:, 1],
+                          odd_idx[:, 1],
+                          odd_idx[:, 2],
+                          odd_idx[:, 1],
+                          faces[:, 2],
+                          odd_idx[:, 0],
+                          odd_idx[:, 1],
+                          odd_idx[:, 2]]).reshape((-1, 3))
+
+      # stack the new even vertices and odd vertices
+      new_vertices = np.vstack((even, odd))
+
+      return new_vertices, new_faces
     
-    # set interior, boundary mask for unique edges
-    edge_bound_mask = np.zeros(len(edges), dtype=bool)
-    edge_bound_mask[edge_bound] = True 
-    edge_bound_mask = edge_bound_mask[unique]
-    edge_inter_mask = ~edge_bound_mask
-    
-    # find the opposite face for each edge
-    edge_pair = np.zeros(len(edges)).astype(int)
-    edge_pair[edge_inter[:, 0]] = edge_inter[:, 1]
-    edge_pair[edge_inter[:, 1]] = edge_inter[:, 0]
-    opposite_face1 = edges_face[unique]
-    opposite_face2 = edges_face[edge_pair[unique]]
-
-    # set odd vertices to the middle of each edge (default as boundary case).
-    odd = vertices[edges[unique]].mean(axis=1)
-    # modify the odd vertices for the interior case
-    e = edges[unique[edge_inter_mask]] 
-    e_v0 = vertices[e][:, 0]
-    e_v1 = vertices[e][:, 1]
-    e_f0 = faces[opposite_face1[edge_inter_mask]]
-    e_f1 = faces[opposite_face2[edge_inter_mask]]
-    e_v2_idx = e_f0[~(e_f0[:, :, None] == e[:, None, :]).any(-1)]
-    e_v3_idx = e_f1[~(e_f1[:, :, None] == e[:, None, :]).any(-1)]
-    e_v2 = vertices[e_v2_idx]
-    e_v3 = vertices[e_v3_idx]
-    odd[edge_inter_mask] = 3/8 * (e_v0 + e_v1) + 1/8 * (e_v2 + e_v3)
-
-    # find vertex neighbors of each vertex 
-    neighbors = graph.neighbors(edges=edges[unique], max_index=len(vertices))
-    # convert list type of array into a fixed-shaped numpy array (set -1 to empties)
-    neighbors = np.array(list(zip_longest(*neighbors, fillvalue=-1))).T
-    # if the neighbor has -1 index, its point is (0, 0, 0), so that
-    # it is not included in the summation of neighbors when calculating the even values
-    vertices_ = np.vstack([vertices, [0, 0, 0]])
-    # number of neighbors
-    k = (neighbors + 1).astype(bool).sum(-1)
-    
-    # calculate even vertices for the interior case
-    even = np.zeros_like(vertices)
-    beta = 1/k * (5/8 - (3/8 + 1/4 * np.cos(2 * np.pi / k)) ** 2)
-    even = beta[:, None] * vertices_[neighbors].sum(1) + (1 - k[:, None] * beta[:, None]) * vertices
-
-    # calculate even vertices for the boundary case
-    if True in edge_bound_mask: 
-      # boundary vertices from boundary edges
-      vrt_bound_mask = np.zeros(len(vertices), dtype=bool)
-      vrt_bound_mask[np.unique(edges[unique][~edge_inter_mask])] = True 
-      # one boundary vertex has two neighbor boundary vertices (set others as -1)
-      boundary_neighbors = neighbors[vrt_bound_mask] 
-      boundary_neighbors[~vrt_bound_mask[neighbors[vrt_bound_mask]]] = -1
-      even[vrt_bound_mask] = 1/8 * vertices_[boundary_neighbors].sum(1) + 3/4 * vertices[vrt_bound_mask]
-
-    # the new faces with odd vertices
-    odd_idx = inverse.reshape((-1, 3)) + len(vertices)
-    new_faces = np.column_stack([faces[:, 0],
-                        odd_idx[:, 0],
-                        odd_idx[:, 2],
-                        odd_idx[:, 0],
-                        faces[:, 1],
-                        odd_idx[:, 1],
-                        odd_idx[:, 2],
-                        odd_idx[:, 1],
-                        faces[:, 2],
-                        odd_idx[:, 0],
-                        odd_idx[:, 1],
-                        odd_idx[:, 2]]).reshape((-1, 3))
-
-    # stack the new even vertices and odd vertices
-    new_vertices = np.vstack((even, odd))
-
-    return new_vertices, new_faces
+    for _index in range(iterations):
+      vertices, faces = _subdivide(vertices, faces)
+    return vertices, faces
