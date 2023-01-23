21585f99e7093a3ab128bd09cf7822cfab36df67
Michael Dawson-Haggerty <mikedh@kerfed.com>
2023-01-23 15:20:55 -0500

    fix shader

--- trimesh/viewer/shader.py
+++ trimesh/viewer/shader.py
@@ -35,27 +35,22 @@ import pyglet.gl as gl  # NOQA
 _SMOOTH_MAX_FACES = 100000
 
 vertex_source = """#version 300 es
-    in vec2 position;
-    in vec4 colors;
-    out vec4 vertex_colors;
-
-    uniform mat4 projection;
-
-    void main()
-    {
-        gl_Position = projection * vec4(position, 0.0, 1.0);
-        vertex_colors = colors;
-    }
-"""
+in vec4 VertexPosition, VertexColor;      
+uniform float RadianAngle;
+out vec4     TriangleColor;    
+mat2 rotation = mat2(0.0, 0.0, 0.0, 0.0);
+void main() {
+gl_Position = mat4(rotation)*VertexPosition;
+TriangleColor = VertexColor;
+}"""
 
 fragment_source = """#version 300 es
-    in vec4 vertex_colors;
-    out vec4 final_color;
-
-    void main()
-    {
-        final_color = vertex_colors;
-    }
+precision mediump float;
+in vec4   TriangleColor;
+out vec4 FragColor;    
+void main() {          
+FragColor = TriangleColor;
+};
 """
 from pyglet.graphics.shader import Shader, ShaderProgram
 
