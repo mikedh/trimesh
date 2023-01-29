ba5d6ec504157410aed1a772d697f5fb519726d7
Michael Dawson-Haggerty <mikedh@kerfed.com>
2023-01-28 19:21:52 -0500

    try test

--- setup.py
+++ setup.py
@@ -68,7 +68,12 @@ requirements_test = set(['pytest',       # run all unit tests
                          'pytest-cov',   # coverage plugin
                          'pyinstrument',  # profile code
                          'coveralls',    # report coverage stats
+                         'autopep8',     # check and autoformat
+                         'flake8',           # static code analysis
+                         'flake8-pyproject', # use new config format
+                         'flake8-no-implicit-concat', # concat rules
                          'ezdxf'])       # use as a validator for exports
+
 
 # Python 2.7 and 3.4 support has been dropped from packages
 # version lock those packages here so install succeeds
