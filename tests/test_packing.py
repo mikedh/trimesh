6a2f03a75a95769edbcb39a78d386b262645cde0
Michael Dawson-Haggerty <mikedh@kerfed.com>
2023-02-05 14:55:56 -0500

    add 3D test case

--- tests/test_packing.py
+++ tests/test_packing.py
@@ -4,6 +4,32 @@ except BaseException:
     import generic as g
 
 
+def assert_no_overlap(bounds, epsilon=1e-8):
+    """
+    Assert that a list of axis-aligned bounding boxes
+    contains no overlaps using `rtree`.
+
+    Parameters
+    ------------
+    bounds : (n, 2, dimension) float
+      Axis aligned bounding boxes
+    epsilon : float
+      Amount to shrink AABB to avoid spurious floating
+      point hits.
+
+    Raises
+    --------------
+    AssertionError
+      If any of `bounds` hit each other.
+    """
+    # pad AABB by epsilon for deterministic intersections
+    padded = g.np.array(bounds) + g.np.reshape([epsilon, -epsilon], (1, 2, 1))
+    tree = g.trimesh.util.bounds_tree(padded)
+    # every returned AABB should not overlap with any other AABB
+    assert all(set(tree.intersection(current.ravel())) ==
+               {i} for i, current in enumerate(bounds))
+
+
 class PackingTest(g.unittest.TestCase):
 
     def setUp(self):
@@ -26,6 +52,26 @@ class PackingTest(g.unittest.TestCase):
         assert count == len(paths)
         # splitting should result in the right number of paths
         assert count == len(r.split())
+
+    def test_3D(self):
+        from trimesh.path import packing
+        e = g.np.array([[14., 14., 0.125],
+                        [13.84376457, 13.84376457, 0.25],
+                        [14., 14., 0.125],
+                        [12.00000057, 12.00000057, 0.25],
+                        [14., 14., 0.125],
+                        [12.83700787, 12.83700787, 0.375],
+                        [12.83700787, 12.83700787, 0.125],
+                        [14., 14., 0.625],
+                        [1.9999977, 1.9999509, 0.25],
+                        [0.87481696, 0.87463294, 0.05],
+                        [0.99955503, 0.99911677, 0.1875]])
+
+        # try packing these 3D boxes
+        bounds, consume = packing.rectangles_single(e)
+        assert consume.all()
+        # assert all bounds are well constructed
+        assert_no_overlap(bounds)
 
 
 if __name__ == '__main__':
