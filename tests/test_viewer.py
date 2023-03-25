653eb6191f4affc326e5f5c4b90209904375f7fb
Michael Dawson-Haggerty <mikedh@kerfed.com>
2023-03-25 14:57:33 -0400

    move window conf into early exit

--- tests/test_viewer.py
+++ tests/test_viewer.py
@@ -9,6 +9,9 @@ class ViewerTest(g.unittest.TestCase):
     def test_viewer(self):
         try:
             from pyglet import gl
+            # set a GL config that fixes a depth buffer issue in xvfb
+            # this should raise an exception if pyglet can't get a library
+            window_conf = gl.Config(double_buffer=True, depth_size=24)
         except BaseException:
             return
 
@@ -19,15 +22,13 @@ class ViewerTest(g.unittest.TestCase):
         # scene = trimesh.scene.Scene(mesh)
         scene = mesh.scene()
 
-        # set a GL config that fixes a depth buffer issue in xvfb
-        window_conf = gl.Config(double_buffer=True, depth_size=24)
         # run the actual render call
         png = scene.save_image(resolution=[1920, 1080],
                                window_conf=window_conf)
 
         assert len(png) > 0
 
-        
+
 if __name__ == '__main__':
     g.trimesh.util.attach_to_log()
     g.unittest.main()
