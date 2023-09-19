7c251e02bfc2f4625338622ee200214c0b1eb573
Michael Dawson-Haggerty <mikedh@kerfed.com>
2023-09-19 16:15:36 -0400

    remove floatlike

--- tests/test_typed.py
+++ tests/test_typed.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-from trimesh.typed import ArrayLike, FloatLike, NDArray, float64, int64
+from trimesh.typed import ArrayLike, NDArray, float64, int64
 
 
 # see if we pass mypy
-def _check(values: ArrayLike[FloatLike]) -> NDArray[int64]:
+def _check(values: ArrayLike) -> NDArray[int64]:
     return (np.array(values, dtype=float64) * 100).astype(int64)
 
 def _run() -> NDArray[int64]:
