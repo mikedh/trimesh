642241eadfd776880dedd3340076c9d59e884a3c
Michael Dawson-Haggerty <mikedh@kerfed.com>
2023-09-19 21:54:22 -0400

    make version.py more complicated

--- trimesh/version.py
+++ trimesh/version.py
@@ -1,13 +1,65 @@
-# get the version trimesh was installed with from metadata
-try:
-    # Python >= 3.8
-    from importlib.metadata import version
-    __version__ = version('trimesh')
-except BaseException:
-    # Python < 3.8
-    from pkg_resources import get_distribution
-    __version__ = get_distribution('trimesh').version
+"""
+# version.py
 
-if __name__ == '__main__':
+Get the current version from package metadata or pyproject.toml
+if everything else fails.
+"""
+
+
+def _get_version():
+    """
+    Try all our methods to get the version.
+    """
+    for method in [_importlib, _pkgresources, _pyproject]:
+        try:
+            return method()
+        except BaseException:
+            pass
+    return None
+
+
+def _importlib() -> str:
+    """
+    Get the version string using package metadata on Python >= 3.8
+    """
+
+    from importlib.metadata import version
+
+    return version("trimesh")
+
+
+def _pkgresources() -> str:
+    """
+    Get the version string using package metadata on Python < 3.8
+    """
+    from pkg_resources import get_distribution
+
+    return get_distribution("trimesh").version
+
+
+def _pyproject() -> str:
+    """
+    Get the version string from the pyproject.toml file.
+    """
+    import json
+    import os
+
+    # use a path relative to this file
+    pyproject = os.path.abspath(
+        os.path.join(
+            os.path.dirname(os.path.abspath(os.path.expanduser(__file__))),
+            "..",
+            "pyproject.toml",
+        )
+    )
+    with open(pyproject) as f:
+        # json.loads cleans up the string and removes the quotes
+        return next(json.loads(L.split("=")[1]) for L in f if "version" in L)
+
+
+# try all our tricks
+__version__ = _get_version()
+
+if __name__ == "__main__":
     # print version if run directly i.e. in a CI script
     print(__version__)
