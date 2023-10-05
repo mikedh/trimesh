ca994d09cb6c39ef89a1d59181b41f399212ebbb
Michael Dawson-Haggerty <mikedh@kerfed.com>
2023-10-05 15:05:58 -0400

    combine version logic into one function

--- trimesh/version.py
+++ trimesh/version.py
@@ -4,57 +4,51 @@
 Get the current version from package metadata or pyproject.toml
 if everything else fails.
 """
+import json
+import os
+from typing import Optional
 
 
-def _get_version():
+def _get_version() -> Optional[str]:
     """
     Try all our methods to get the version.
     """
-    for method in [_importlib, _pkgresources, _pyproject]:
-        try:
-            return method()
-        except BaseException:
-            pass
-    return None
 
+    try:
+        # Get the version string using package metadata on Python >= 3.8
+        from importlib.metadata import version
 
-def _importlib() -> str:
-    """
-    Get the version string using package metadata on Python >= 3.8
-    """
+        return version("trimesh")
+    except BaseException:
+        pass
 
-    from importlib.metadata import version
+    try:
+        # Get the version string using package metadata on Python < 3.8
+        from pkg_resources import get_distribution
 
-    return version("trimesh")
+        return get_distribution("trimesh").version
+    except BaseException:
+        pass
 
-
-def _pkgresources() -> str:
-    """
-    Get the version string using package metadata on Python < 3.8
-    """
-    from pkg_resources import get_distribution
-
-    return get_distribution("trimesh").version
-
-
-def _pyproject() -> str:
-    """
-    Get the version string from the pyproject.toml file.
-    """
-    import json
-    import os
-
-    # use a path relative to this file
-    pyproject = os.path.abspath(
-        os.path.join(
-            os.path.dirname(os.path.abspath(os.path.expanduser(__file__))),
-            "..",
-            "pyproject.toml",
+    try:
+        # Get the version string from the pyproject.toml file using
+        # relative paths. This will be the only option if the library
+        # has not been installed (i.e. in a CI or container environment)
+        pyproject = os.path.abspath(
+            os.path.join(
+                os.path.dirname(os.path.abspath(os.path.expanduser(__file__))),
+                "..",
+                "pyproject.toml",
+            )
         )
-    )
-    with open(pyproject) as f:
-        # json.loads cleans up the string and removes the quotes
-        return next(json.loads(L.split("=")[1]) for L in f if "version" in L)
+        with open(pyproject) as f:
+            # json.loads cleans up the string and removes the quotes
+            # this logic requires the first use of "version" be the actual version
+            return next(json.loads(L.split("=")[1]) for L in f if "version" in L)
+    except BaseException:
+        pass
+
+    return None
 
 
 # try all our tricks
