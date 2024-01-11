1f481aac98d8176847d6fe5f3c206854ae704d1a
Luca Grementieri <luca@promaton.com>
2024-01-11 11:03:51 +0100

    Update transformations.py

--- trimesh/transformations.py
+++ trimesh/transformations.py
@@ -1933,6 +1933,7 @@ def angle_between_vectors(v0, v1, directed=True, axis=0):
     v1 = np.array(v1, dtype=np.float64, copy=False)
     dot = np.sum(v0 * v1, axis=axis)
     dot /= vector_norm(v0, axis=axis) * vector_norm(v1, axis=axis)
+    dot = np.clip(dot, -1.0, 1.0)
     return np.arccos(dot if directed else np.fabs(dot))
 
 
