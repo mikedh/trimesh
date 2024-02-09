72ad88d323dba10a4d126b6842a370b63240b2f9
Erkhembayaar <71809096+Erkhembayaar@users.noreply.github.com>
2024-02-09 11:24:54 +0800

    Update blender.py
    condition check to cond is True and removed some trailing whitespaces

--- trimesh/interfaces/blender.py
+++ trimesh/interfaces/blender.py
@@ -35,14 +35,12 @@ exists = _blender_executable is not None
 def boolean(meshes, operation="difference", solver_options=False, use_self=False, debug=False):
     """
     Run a boolean operation with multiple meshes using Blenderoy.
-    
     Parameters:
     - meshes: List of mesh file paths to be processed.
     - operation: Type of boolean operation ("difference", "union", "intersect").
     - solver_options: Solver option for the boolean operation, True == 'Exact'
     - use_self: Boolean indicating whether to consider self-intersections.
     - debug: If True, run in debug mode to provide additional output for troubleshooting.
-    
     Returns:
     - The result of the boolean operation on the provided meshes.
     """
@@ -52,11 +50,10 @@ def boolean(meshes, operation="difference", solver_options=False, use_self=False
     if operation == "INTERSECTION":
         operation = "INTERSECT"
 
-    if solver_options == True:
+    if solver_options is True:
         solver_options = 'EXACT'
     else: 
-        solver_options = 'FAST'
-        
+        solver_options = 'FAST'    
     # get the template from our resources folder
     template = resources.get("templates/blender_boolean.py.tmpl")
     script = template.replace("$OPERATION", operation)
