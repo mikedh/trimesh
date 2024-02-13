2988e2f224d977cd791387abc0ec12446a720c2f
Ruoxi <flandre@scarletx.cn>
2024-02-13 14:51:31 +0800

    Fix reshape in `get_specular_glossiness`.

--- trimesh/visual/gloss.py
+++ trimesh/visual/gloss.py
@@ -180,9 +180,9 @@ def specular_to_pbr(
                 or specularGlossinessTexture.shape[-1]
             ) == 1:
                 # use the one channel as a multiplier for specular and glossiness
-                specularTexture = glossinessTexture = specularGlossinessTexture[
-                    ..., np.newaxis
-                ]
+                specularTexture = glossinessTexture = specularGlossinessTexture.reshape(
+                    specularGlossinessTexture.shape[0], specularGlossinessTexture.shape[1], 1
+                )
             elif specularGlossinessTexture.shape[-1] == 3:
                 # all channels are specular, glossiness is only a factor
                 specularTexture = specularGlossinessTexture[..., :3]
