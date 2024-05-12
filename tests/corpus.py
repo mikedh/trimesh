b48254425e8f7d6b2732d28107d54b7c6c61c9fd
Michael Dawson-Haggerty <mik3dh@gmail.com>
2024-05-12 17:31:52 -0400

    revert the assimp changes for followup

--- tests/corpus.py
+++ tests/corpus.py
@@ -24,7 +24,7 @@ available.difference_update(
     ]
 )
 # remove loaders we don't care about
-available.difference_update({"json", "obj", "dae", "zae"})
+available.difference_update({"json", "dae", "zae"})
 available.update({"dxf", "svg"})
 
 
@@ -176,20 +176,13 @@ if __name__ == "__main__":
     with Profiler() as P:
         # check the assimp corpus, about 50mb
         report = on_repo(
-            repo="assimp/assimp", commit="c9537394876931880b6d71d27519c0baeb637c46"
+            repo="assimp/assimp", commit="c2967cf79acdc4cd48ecb0729e2733bf45b38a6f"
         )
-        # check the older gltf-sample-models, about 1gb
+        # check the gltf-sample-models, about 1gb
         report.update(
             on_repo(
                 repo="KhronosGroup/glTF-Sample-Models",
                 commit="8e9a5a6ad1a2790e2333e3eb48a1ee39f9e0e31b",
-            )
-        )
-        # check the samples on the new repo
-        report.update(
-            on_repo(
-                repo="KhronosGroup/glTF-Sample-Assets",
-                commit="cfbe2f9ac259490855940ff85feb5b4b02386046",
             )
         )
 
