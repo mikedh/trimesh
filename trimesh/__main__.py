ba0afbd13e7998b94093a957dc8dafa4f7565775
Michael Dawson-Haggerty <mikedh@kerfed.com>
2024-09-27 12:22:29 -0400

    add trimesh -i CLI helper

--- trimesh/__main__.py
+++ trimesh/__main__.py
@@ -17,9 +17,42 @@ def main():
     from .exchange.load import load
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("file_name")
+    parser.add_argument("file_name", nargs="?")
+
+    parser.add_argument(
+        "-i",
+        "--interact",
+        action="store_true",
+        help="Get an interactive terminal with trimesh and loaded geometry",
+    )
+    parser.add_argument("-e", "--export", help="Export a loaded geometry to a new file.")
+
     args = parser.parse_args()
-    load(args.file_name).show()
+
+    if args.file_name is None:
+        scene = None
+    else:
+        scene = load(args.file_name)
+
+    if args.export is not None:
+        scene.export(args.export)
+
+    if args.interact:
+        return interactive(scene)
+
+
+def interactive(scene):
+    """
+    Run an interactive session with a loaded scene and trimesh.
+
+    This uses the standard library `code.InteractiveConsole`
+    """
+    local = locals()
+
+    from code import InteractiveConsole
+
+    # filter out junk variables.
+    InteractiveConsole(locals={k: v for k, v in local.items() if k != "local"}).interact()
 
 
 if __name__ == "__main__":
