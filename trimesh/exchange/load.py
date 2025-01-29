907d7dd15ba3808c00c7a597f30974b93db43b5f
Michael Dawson-Haggerty <mikedh@kerfed.com>
2025-01-29 13:30:45 -0500

    replicate old kwarg passthrough behavior

--- trimesh/exchange/load.py
+++ trimesh/exchange/load.py
@@ -1,5 +1,6 @@
 import json
 import os
+from copy import deepcopy
 
 import numpy as np
 
@@ -210,15 +211,16 @@ def load_scene(
             )
         elif arg.file_type in mesh_loaders:
             # use mesh loader
-            loaded = _load_kwargs(
-                mesh_loaders[arg.file_type](
+            parsed = deepcopy(kwargs)
+            parsed.update(mesh_loaders[arg.file_type](
                     file_obj=arg.file_obj,
                     file_type=arg.file_type,
                     resolver=arg.resolver,
                     metadata=metadata,
                     **kwargs,
-                )
-            )
+                ))
+            loaded = _load_kwargs(**parsed)
+
         elif arg.file_type in compressed_loaders:
             # for archives, like ZIP files
             loaded = _load_compressed(arg.file_obj, file_type=arg.file_type, **kwargs)
