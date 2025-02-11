719eb2c8975b75e1ac52d47c3c4cfb9fc186ab89
Michael Dawson-Haggerty <mikedh@cue.local>
2025-02-11 12:42:19 -0500

    ruff format and version bump

--- tests/test_exchange.py
+++ tests/test_exchange.py
@@ -1,7 +1,7 @@
-from trimesh.path.exchange.misc import linestrings_to_path
-from trimesh.path.exchange.load import load_path
-
 from shapely.geometry import LineString, MultiLineString
+
+from trimesh.path.exchange.load import load_path
+from trimesh.path.exchange.misc import linestrings_to_path
 
 
 def test_linestrings_to_path():
@@ -14,10 +14,9 @@ def test_linestrings_to_path():
 
 
 def test_multilinestrings_to_path():
-    line = MultiLineString([
-        LineString([(0, 0), (1, 0), (2, 0)]),
-        LineString([(0, 1), (1, 1), (2, 1)])
-    ])
+    line = MultiLineString(
+        [LineString([(0, 0), (1, 0), (2, 0)]), LineString([(0, 1), (1, 1), (2, 1)])]
+    )
 
     result = linestrings_to_path(line)
 
@@ -26,10 +25,9 @@ def test_multilinestrings_to_path():
 
 
 def test_load_path_with_multilinestrings():
-    line = MultiLineString([
-        LineString([(0, 0), (1, 0), (2, 0)]),
-        LineString([(0, 1), (1, 1), (2, 1)])
-    ])
+    line = MultiLineString(
+        [LineString([(0, 0), (1, 0), (2, 0)]), LineString([(0, 1), (1, 1), (2, 1)])]
+    )
 
     result = load_path(line)
 
