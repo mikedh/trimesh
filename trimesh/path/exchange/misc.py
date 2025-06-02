c9d604394be2ec542885104af44d6819d72849f5
Tom Aubier <tom.aubier@inserm.fr>
2025-06-02 11:38:55 +0200

    Fixes a bug where dict_to_path raises an AttributeError when defining Line objects

--- trimesh/path/exchange/misc.py
+++ trimesh/path/exchange/misc.py
@@ -29,8 +29,13 @@ def dict_to_path(as_dict):
     entities = [None] * len(as_dict["entities"])
     # run constructor for dict kwargs
     for entity_index, entity in enumerate(as_dict["entities"]):
+      if entity["type"] == 'Line':
         entities[entity_index] = loaders[entity["type"]](
-            points=entity["points"], closed=entity["closed"]
+          points=entity["points"]
+        )
+      else:
+        entities[entity_index] = loaders[entity["type"]](
+          points=entity["points"], closed=entity["closed"]
         )
     result["entities"] = entities
 
