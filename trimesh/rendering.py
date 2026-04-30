6796148517f2fd227ad4b80cef9bd993360ce0fc
Michael Dawson-Haggerty <mikedh@kerfed.com>
2026-04-30 15:52:03 -0400

    add a deprecation date for rendering

--- trimesh/rendering.py
+++ trimesh/rendering.py
@@ -2,4 +2,6 @@ from logging import getLogger
 
 from .viewer.pyglet1.conversion import *
 
-getLogger(__name__).warning("`trimesh.rendering` is depricated`")
+getLogger(__name__).warning(
+    "`trimesh.rendering` is Pyglet1-specific, and is deprecated. This compatibility shim may be removed any time after June 2027"
+)
